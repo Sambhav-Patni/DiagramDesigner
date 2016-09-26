86089d7e4d3ad4bd6f1951d7596d7197f242c07e
Source Of Arising Mentality <sambhav181@gmail.com>
2016-09-26 17:12:50 +0530

    Add files via upload

---
+++ App_Start/WebApiConfig.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+
+namespace DDWebAPI
+{
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}/{all}",
+                defaults: new { id = RouteParameter.Optional, all = RouteParameter.Optional }
+            );
+        }
+    }
+}
